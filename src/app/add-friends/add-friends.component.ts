f61b85e3e50919aadc032f1a6b8dfa5b2c71099f
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-30 10:14:00 +0530

    added

--- src/app/add-friends/add-friends.component.ts
+++ src/app/add-friends/add-friends.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-friends',
@@ -10,11 +11,26 @@ name=""
 friendName=""
 friendNickName=""
 DescribeYourFriend=""
-
+constructor(private api:ApiService){}
 readValues=()=>
 {
  let data:any={"name":this.name,"friendName":this.friendName,"friendNickName":this.friendNickName,"DescribeYourFriend":this.DescribeYourFriend}
 console.log(data)
+this.api.addFriends(data).subscribe(
+  (response:any)=>
+  {
+    console.log(response)
+    if (response.status=="success") {
+      alert("Friend added successfully")
+      this.name=""
+      this.friendName=""
+      this.friendNickName=""
+      this.DescribeYourFriend=""
+    } else {
+      alert("Something went wrong")
+    }
+  }
+)
 }
 
 }
